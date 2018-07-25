2f1c9a28bcc390f292b6d0193c86a32e6e23a918
Keeliana Marler <kmmarler2@hotmail.com>
2018-07-24 21:59:30 -0600

    update data cleaner

--- src/Utilities/SetCleaner/setCleaner.js
+++ src/Utilities/SetCleaner/setCleaner.js
@@ -1,26 +1,12 @@
 export const CleanSet = data => {
   const sets = data.setlist.map(set => {
-    return (
-      <div className='card_div'>
-        <h2 className='artist'>{props.artist}</h2>
-        <h2 className='venue'>{props.venue}</h2>
-        <h2 className='city'>{props.city}</h2>
-        <h2 className='date'>{props.date}</h2>
-        <ul>{props.setlist}</ul>
-      </div>
-      <div
-        className="card_image"
-        style={{
-          backgroundImage: `url(https://image.tmdb.org/t/p/w500${poster_path})`
-        }}
-      >
-        <div className="rating_div">
-          <h3 className="card_rating">{vote_average}</h3>
-        </div>
-      </div>
-      {favoriteIds.includes(movie_id) ? deleteButton : favoriteButton}
-    </div>
-      )
+    return {
+      artist: set.artist.name,
+      date: set.eventDate,
+      venue: set.venue.name,
+      city: set.venue.city.name,
+      set: set.sets.set[0] ? set.sets.set[0].song : null
+    }
   })
   return sets
 }
